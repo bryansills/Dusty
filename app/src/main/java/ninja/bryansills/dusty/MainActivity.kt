6396d2a366d5ec56280e28a36ac0dce430eb7dbb
Bryan Sills <bryan@bryansills.com>
2019-06-23 22:16:04 -0500

    Set up authentication for Android app

--- app/src/main/java/ninja/bryansills/dusty/MainActivity.kt
+++ app/src/main/java/ninja/bryansills/dusty/MainActivity.kt
@@ -1,17 +1,46 @@
 package ninja.bryansills.dusty
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import ninja.bryansills.dusty.network.Buttz
+import android.widget.Button
+import com.spotify.sdk.android.authentication.AuthenticationClient
+import com.spotify.sdk.android.authentication.AuthenticationRequest
+import com.spotify.sdk.android.authentication.AuthenticationResponse
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val CLIENT_ID = BuildConfig.SPOTIFY_CLIENT_ID
+    private val REDIRECT_URI = BuildConfig.SPOTIFY_REDIRECT_URI
+    private val REQUEST_CODE = 5907
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttz = Buttz("blarg")
-        Log.d("buttz", buttz.toString())
+        val button = findViewById<Button>(R.id.login_button)
+        button.setOnClickListener {
+            val builder = AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
+            builder.setScopes(arrayOf("user-read-private", "streaming"))
+            val request = builder.build()
+
+            AuthenticationClient.openLoginActivity(this@MainActivity, REQUEST_CODE, request)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+
+        if (requestCode == REQUEST_CODE) {
+            val response = AuthenticationClient.getResponse(resultCode, intent)
+            if (response.type == AuthenticationResponse.Type.TOKEN) {
+                val accessToken = response.accessToken
+                Log.d("blarg", accessToken)
+            }
+        }
     }
 }
