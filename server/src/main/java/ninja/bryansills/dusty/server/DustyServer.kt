8f5b23700d8cd3846b3f79a8f678a6f6c866a80b
bryansills <bryan@bryansills.com>
2021-02-21 21:18:13 -0600

    prettify callback page

--- server/src/main/java/ninja/bryansills/dusty/server/DustyServer.kt
+++ server/src/main/java/ninja/bryansills/dusty/server/DustyServer.kt
@@ -2,6 +2,7 @@ package ninja.bryansills.dusty.server
 
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.html.respondHtml
 import io.ktor.http.ContentType
 import io.ktor.http.ParametersBuilder
 import io.ktor.http.URLBuilder
@@ -11,6 +12,7 @@ import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.util.*
+import kotlinx.html.*
 import ninja.bryansills.dusty.network.auth.NetworkAuthService
 import ninja.bryansills.dusty.network.auth.RealNetworkAuthService
 
@@ -30,17 +32,60 @@ fun Application.module(testing: Boolean = false) {
 
     routing {
         get("/") {
-            log.info("BLARG ROOT")
             call.respondText("HELLO BUTTZ FROM BUILT DOCKER!", contentType = ContentType.Text.Plain)
         }
         get("/callback") {
             val queryParameters = call.request.queryParameters
-            log.info("BLARG QUERY PARAMS ${queryParameters.toMap()}")
             val authorizationCode = queryParameters.getOrFail("code")
-            log.info("BLARG AUTH CODE $authorizationCode")
             val tokenResponse = networkAuthService.requestTokens(authorizationCode)
-            log.info("BLARG TOKEN RESPONSE $tokenResponse")
-            call.respondText(tokenResponse.toString(), ContentType.Text.Plain)
+            call.respondHtml {
+                head {
+                    title { +"Signing in..." }
+                    style {
+                        unsafe {
+                            raw("""
+                                body {
+                                    height: 100%;
+                                    margin: 0;
+                                    display: flex;
+                                    justify-content: center;
+                                    align-items: center;
+                                    background-color: #212121;
+                                }
+                            """.trimIndent())
+                        }
+                    }
+                }
+                body {
+                    svg {
+                        attributes["width"] = "100px"
+                        attributes["height"] = "100px"
+                        attributes["viewBox"] = "0 0 40 40"
+                        unsafe {
+                            raw("""
+                                <path opacity="0.2" fill="#FFF" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
+                                    s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
+                                    c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
+                                <path opacity="0.9" fill="#FFF" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
+                                    C22.32,8.481,24.301,9.057,26.013,10.047z">
+                                    <animateTransform attributeType="xml"
+                                        attributeName="transform"
+                                        type="rotate"
+                                        from="0 20 20"
+                                        to="360 20 20"
+                                        dur="0.8s"
+                                        repeatCount="indefinite"/>
+                                </path>
+                            """.trimIndent())
+                        }
+                    }
+                    script {
+                        unsafe {
+                            raw("dusty.passToken(${tokenResponse.accessToken}, ${tokenResponse.refreshToken}, ${tokenResponse.expiresIn});")
+                        }
+                    }
+                }
+            }
         }
         get("/start") {
             val uriBuilder = URLBuilder(
