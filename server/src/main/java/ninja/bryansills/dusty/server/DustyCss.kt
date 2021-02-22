973b3a1c8038ab7e7e68022119fad821db0df157
bryansills <bryan@bryansills.com>
2021-02-21 23:17:50 -0600

    retry styling stuff

---
+++ server/src/main/java/ninja/bryansills/dusty/server/DustyCss.kt
@@ -0,0 +1,10 @@
+package ninja.bryansills.dusty.server
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import kotlinx.css.CSSBuilder
+
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
--- server/src/main/java/ninja/bryansills/dusty/server/DustyServer.kt
+++ server/src/main/java/ninja/bryansills/dusty/server/DustyServer.kt
@@ -12,6 +12,8 @@ import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.util.*
+import kotlinx.css.*
+import kotlinx.css.LinearDimension.Companion.maxContent
 import kotlinx.html.*
 import ninja.bryansills.dusty.network.auth.NetworkAuthService
 import ninja.bryansills.dusty.network.auth.RealNetworkAuthService
@@ -34,6 +36,21 @@ fun Application.module(testing: Boolean = false) {
         get("/") {
             call.respondText("HELLO BUTTZ FROM BUILT DOCKER!", contentType = ContentType.Text.Plain)
         }
+        get("/styles.css") {
+            call.respondCss {
+                html {
+                    height = 100.pct
+                }
+                body {
+                    margin(0.px)
+                    height = 100.pct
+                    display = Display.flex
+                    justifyContent = JustifyContent.center
+                    alignItems = Align.center
+                    backgroundColor = Color("#212121")
+                }
+            }
+        }
         get("/callback") {
             val queryParameters = call.request.queryParameters
             val authorizationCode = queryParameters.getOrFail("code")
@@ -41,20 +58,7 @@ fun Application.module(testing: Boolean = false) {
             call.respondHtml {
                 head {
                     title { +"Signing in..." }
-                    style {
-                        unsafe {
-                            raw("""
-                                body {
-                                    height: 100%;
-                                    margin: 0;
-                                    display: flex;
-                                    justify-content: center;
-                                    align-items: center;
-                                    background-color: #212121;
-                                }
-                            """.trimIndent())
-                        }
-                    }
+                    link(rel = "stylesheet", href = "/styles.css", type = "text/css")
                 }
                 body {
                     svg {
@@ -62,26 +66,32 @@ fun Application.module(testing: Boolean = false) {
                         attributes["height"] = "100px"
                         attributes["viewBox"] = "0 0 40 40"
                         unsafe {
-                            raw("""
-                                <path opacity="0.2" fill="#FFF" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
-                                    s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
-                                    c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
-                                <path opacity="0.9" fill="#FFF" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
-                                    C22.32,8.481,24.301,9.057,26.013,10.047z">
-                                    <animateTransform attributeType="xml"
-                                        attributeName="transform"
-                                        type="rotate"
-                                        from="0 20 20"
-                                        to="360 20 20"
-                                        dur="0.8s"
-                                        repeatCount="indefinite"/>
-                                </path>
-                            """.trimIndent())
+                            raw(
+                                """
+                                    <path opacity="0.2" fill="#FFF" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
+                                        s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
+                                        c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
+                                    <path opacity="0.9" fill="#FFF" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
+                                        C22.32,8.481,24.301,9.057,26.013,10.047z">
+                                        <animateTransform attributeType="xml"
+                                            attributeName="transform"
+                                            type="rotate"
+                                            from="0 20 20"
+                                            to="360 20 20"
+                                            dur="0.8s"
+                                            repeatCount="indefinite"/>
+                                    </path>
+                                """.trimIndent()
+                            )
                         }
                     }
                     script {
                         unsafe {
-                            raw("dusty.passToken(${tokenResponse.accessToken}, ${tokenResponse.refreshToken}, ${tokenResponse.expiresIn});")
+                            raw(
+                                """
+                                    dusty.passToken("${tokenResponse.accessToken}", "${tokenResponse.refreshToken}", ${tokenResponse.expiresIn});"
+                                """.trimIndent()
+                            )
                         }
                     }
                 }
