edfeb69caf4cbf3490a6bb99b7fa7ba5d67312f8
bryansills <bryan@bryansills.com>
2021-02-22 20:57:53 -0600

    Provide basic login functionality in Android

--- app/src/main/java/ninja/bryansills/dusty/MainActivity.kt
+++ app/src/main/java/ninja/bryansills/dusty/MainActivity.kt
@@ -1,43 +1,28 @@
 package ninja.bryansills.dusty
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.webkit.JavascriptInterface
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import com.spotify.sdk.android.authentication.AuthenticationClient
-import com.spotify.sdk.android.authentication.AuthenticationRequest
-import com.spotify.sdk.android.authentication.AuthenticationResponse
+import ninja.bryansills.dusty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private val CLIENT_ID = BuildConfig.SPOTIFY_CLIENT_ID
-    private val REDIRECT_URI = BuildConfig.SPOTIFY_REDIRECT_URI
-    private val REQUEST_CODE = 5907
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val button = findViewById<Button>(R.id.login_button)
-        button.setOnClickListener {
-            val builder = AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
-            builder.setScopes(arrayOf("user-read-recently-played", "user-library-read"))
-            val request = builder.build()
-
-            AuthenticationClient.openLoginActivity(this@MainActivity, REQUEST_CODE, request)
-        }
+        binding.loginWebView.settings.javaScriptEnabled = true
+        binding.loginWebView.addJavascriptInterface(WebViewJavascriptInterface(), "dusty")
+        binding.loginWebView.webViewClient = WebViewClient()
+        binding.loginWebView.loadUrl("https://dusty-server.herokuapp.com/start")
     }
+}
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
-        super.onActivityResult(requestCode, resultCode, intent)
-
-        if (requestCode == REQUEST_CODE) {
-            val response = AuthenticationClient.getResponse(resultCode, intent)
-            if (response.type == AuthenticationResponse.Type.TOKEN) {
-                val accessToken = response.accessToken
-                Log.d("blarg", accessToken)
-            }
-        }
+class WebViewJavascriptInterface {
+    @JavascriptInterface
+    fun passToken(accessToken: String, refreshToken: String, expiresIn: Int) {
+        Log.d("BLARG", "$accessToken\n$refreshToken\n$expiresIn")
     }
 }
