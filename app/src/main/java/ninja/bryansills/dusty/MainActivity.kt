a755eb8645796a330a1df8d154bd47826fb648c2
bryansills <bryan@bryansills.com>
2021-12-26 11:12:37 -0600

    Migrate to Compose

--- app/src/main/java/ninja/bryansills/dusty/MainActivity.kt
+++ app/src/main/java/ninja/bryansills/dusty/MainActivity.kt
@@ -2,21 +2,44 @@ package ninja.bryansills.dusty
 
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
 import android.webkit.JavascriptInterface
+import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
-import ninja.bryansills.dusty.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.viewinterop.AndroidView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            AppTheme {
+                Scaffold(
+                    topBar = { TopAppBar(title = { Text(stringResource(R.string.app_name)) }) }
+                ) {
+                    AndroidView(
+                        factory = { context ->
+                            WebView(context).apply {
+                                layoutParams = ViewGroup.LayoutParams(
+                                    ViewGroup.LayoutParams.MATCH_PARENT,
+                                    ViewGroup.LayoutParams.MATCH_PARENT
+                                )
+                                settings.javaScriptEnabled = true
+                                addJavascriptInterface(WebViewJavascriptInterface(), "dusty")
+                                webViewClient = WebViewClient()
+                                loadUrl("https://dusty-server.herokuapp.com/start")
+                            }
+                        }
+                    )
 
-        binding.loginWebView.settings.javaScriptEnabled = true
-        binding.loginWebView.addJavascriptInterface(WebViewJavascriptInterface(), "dusty")
-        binding.loginWebView.webViewClient = WebViewClient()
-        binding.loginWebView.loadUrl("https://dusty-server.herokuapp.com/start")
+                }
+            }
+        }
     }
 }
 
